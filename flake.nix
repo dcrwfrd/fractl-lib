4f6fb03a475918eca0f71a3bc74707a4caf033a8
Devin Crawford <dcrwfrd@proton.me>
2025-12-27 21:08:48 -0500

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Fractl - A modular NixOS configuration framework";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+    
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      flake.flakeModules.default = ./lib;
+    };
+}
+
